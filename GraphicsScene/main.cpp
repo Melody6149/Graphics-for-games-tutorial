66ed9ac27a627f574f29db6ea7c1b7312e19f037
Melody <reid.eastin@gmail.com>
2020-09-29 11:59:47 -0500

    Got a window to show up

--- GraphicsScene/main.cpp
+++ GraphicsScene/main.cpp
@@ -1,6 +1,45 @@
 #include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
+#include <cstdio>
 
 int main() {
+
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	//Game goes here
+
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "Game",nullptr,nullptr );
+
+
+	if (window == nullptr)
+	{
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+
+	int major = ogl_GetMajorVersion();
+	int minor = ogl_GetMinorVersion();
+	printf("OpenGL version: %i.%i\n", major, minor);
+
+	//Loopgoes here
+	while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
